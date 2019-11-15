5bc4162c5faf510f6a97bc7b75bb3dc6590f3e5b
Vadim Gorbachev <vadim.gorbachev.main@gmail.com>
2019-11-15 21:14:42 +0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Vadim  Gorbachev on 15.11.2019.
+//  Copyright © 2019 Vadim  Gorbachev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
